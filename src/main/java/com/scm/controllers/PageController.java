d916512e7f2b86c8b8338b5f77592a107246c5e1
saurabh <saurabhyamgar157@gmial.com>
2025-05-10 10:04:29 +0530

    all basics of project are saved

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,13 +1,31 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("headline", "Home Page form controller");
+        model.addAttribute("para", "All content is coming fromt the controller via model object");
+        model.addAttribute("Repo", "https://google.com");
         return "home";
     }
+
+    @GetMapping("/about")
+    public String about(){
+       
+        return "about";
+    }
+
+    @GetMapping("/services")
+    public String servicePage(Model model){
+        model.addAttribute("isLogin" , false);
+       
+        return "services";
+    }
+    
 }
